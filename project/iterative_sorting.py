609b8cc1eeac12d159ed3f491e1b94fc73877799
Trevor Lewis <tenzingtitan@gmail.com>
2019-02-19 14:21:59 -0700

    Initial Commit

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -6,7 +6,11 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[cur_index]:
+                cur_index = j
+            if cur_index != i:
+                arr[i], arr[cur_index] = arr[cur_index], arr[i]
 
 
 
