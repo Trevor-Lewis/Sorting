7ff323bbb88d32283fe2c56f0fe6251bb7799fd7
Trevor Lewis <tenzingtitan@gmail.com>
2019-02-19 14:22:15 -0700

    Initial commit

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -24,13 +24,25 @@ def selection_sort( arr ):
 
 # TO-DO: implement the Insertion Sort function below
 def insertion_sort( arr ):
-
+    for i in range(1, len(arr)):
+        current_element = arr[1]
+        j = i -1
+        while j >= 0 and current_element < arr[j]
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = current_element
     return arr
 
 
 # STRETCH: implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    is_sorted = False
+    while not is_sorted:
+        is_sorted: True
+        for i in range(len(arr) - 1)
+            if arr[i] > arr[i + 1]
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                is_sorted = False
     return arr
 
 
